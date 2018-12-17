ef59c35e73299e89532d884b721339ac0b0ad001
Robertas <robertas@unity3d.com>
2018-12-17 11:42:29 +0200

    Initial commit

---
+++ Assets/LetsCount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetsCount : MonoBehaviour {
+
+    public Text field;
+	
+	// Update is called once per frame
+	void Update () {
+        field.text = Time.fixedTime.ToString();
+	}
+}
